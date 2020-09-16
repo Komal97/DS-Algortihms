03e0e79601cb9b993782949d83350bb44f865efb
Komal <komalbansal97@gmail.com>
2020-09-17 00:14:43 +0530

    unique number 3 updated

--- 4. Bitmasking/Python/unique_element_III.py
+++ 4. Bitmasking/Python/unique_element_III.py
@@ -1,7 +1,19 @@
 '''
-Input an array
-In array, each number is occuring thrice except one element, find that unique element
-Eg : 1, 2, 2, 2, 1, 1, 1, 3 --> ans - 3
+https://www.interviewbit.com/problems/single-number-ii/
+Given an array of integers, every element appears thrice except for one which occurs once.
+Find that element which does not appear thrice. 
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Input:
+A = [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+Output:
+4
+Explanation:
+4 occur exactly once
+
+Input:
+A = [0, 0, 0, 1]
+Output:
+1
 '''
 
 def find_3_unique_elements(arr, n):
@@ -24,10 +36,30 @@ def find_3_unique_elements(arr, n):
         p = p<<1
     print(ans)
 
-def main():
-    
-    arr = list(map(int, input().split()))
-    n = len(arr)
-    find_3_unique_elements(arr, n)
+arr = list(map(int, input().split()))
+n = len(arr)
+find_3_unique_elements(arr, n)
 
-main()
+
+
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def singleNumber(self, A):
+        
+        A = list(A)
+        res = 0
+        for i in range(64):
+            j = 0
+            cnt = 0
+            while j < len(A):
+                cnt += (A[j]&1)
+                val = A[j]>>1
+                A[j] = int(val)
+                j += 1
+            res |= ((cnt%3)<<i)
+            
+            
+        return res
+                
