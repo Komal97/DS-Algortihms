fa9a6f13a678c3a60d553661883cd862c2ccc2fe
komal.bansal1 <komal.bansal@gxs.com.sg>
2024-06-30 21:23:07 +0530

    count sort method 2 added (stable sort)

--- 3. Sorting Searching/Python/counting_sort.py
+++ 3. Sorting Searching/Python/counting_sort.py
@@ -1,3 +1,4 @@
+# method -1 
 def countsort(arr, n):
     freq = [0]*10
     
@@ -14,6 +15,38 @@ def countsort(arr, n):
 
     print(arr)
 
+
+# method - 2 --> if we want to keep sequence intact (stable sort)
+def countsort(arr, n):
+    # arr = [9, 6, 3, 5, 3, 4, 3, 9, 6, 4, 6, 5, 8, 9, 9]
+    
+    minVal = min(arr)      # min = 3
+    maxVal = max(arr)      # max = 9 
+
+    rangeVal = maxVal - minVal + 1      # range = 7
+
+    freq = [0 for _ in range (rangeVal)]
+
+    # find frequency  - [3, 2, 2, 3, 0, 1, 4]
+    for num in arr:
+        idx = num - minVal
+        freq[idx] += 1
+
+    # find cumulative sum of frequencies to keep next index  - [3, 5, 7, 10, 10, 11, 15] - indicating indexes starting from 1, not 0
+    sumTillNow = 0
+    for i in range(rangeVal):
+        sumTillNow += freq[i]
+        freq[i] = sumTillNow                
+
+    ans = [0 for _ in range (n)] 
+
+    for i in range(n-1, -1, -1):
+        idx = arr[i] - minVal              # find index where freq of value is being kep
+        ans[freq[idx] - 1] = arr[i]        # put current value into ans array at index represented by fre
+        freq[idx] -= 1                 
+    
+    
+    
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
--- 5. Basic Number Theory/Python/prime(sieve_of_eratostenes).py
+++ 5. Basic Number Theory/Python/prime(sieve_of_eratostenes).py
@@ -3,17 +3,18 @@ Input a number
 Ouput all prime numbers less than a given number
 '''
 def prime_sieve(n):
-    p = [0 for _ in range(n+1)]
+    p = [0 for _ in range(n+1)] # mark all number as non-prime
 
-    for i in range(3, len(p)+1, 2):
+    for i in range(3, len(p)+1, 2): # mark odd number as prime
         p[i] = 1
 
     p[2] = 1
     
+    #iterate over only odd numbers
     for i in range(3, n+1, 2):
-        if p[i]:
+        if p[i]:    
             #for j in range(2*i, n+1, i):
-            for j in range(i*i, n+1, 2*i):
+            for j in range(i*i, n+1, 2*i): # mark all multiples that are not prime, and take jump of 2 because (odd i*i + odd i becomes even) 
                 p[j] = 0
 
     for i in range(len(p)):
